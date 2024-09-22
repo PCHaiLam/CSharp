5a4c6048157b97e761d0905495dcf4f6090702bf
Phan Châu Hải Lâm <phanchauhailam15803@gmail.com>
2024-09-22 17:26:25 +0700

    bt3

---
+++ MVC/ThucHanh/BaiTap2_63134417/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiTap2_63134417.Models
+{
+    public class Class1
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double mark { get; set; }
+    }
+}
---
+++ MVC/ThucHanh/BaiTap3_63134417/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BaiTap3_63134417.MvcApplication" Language="C#" %>
