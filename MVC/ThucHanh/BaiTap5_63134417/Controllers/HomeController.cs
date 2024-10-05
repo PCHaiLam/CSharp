b2f9e066ba71d20eb7098676f442948586d6127f
Phan Châu Hải Lâm <phanchauhailam15803@gmail.com>
2024-10-05 09:18:55 +0700

    //

---
+++ MVC/ThucHanh/BaiTap5_63134417/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using BaiTap5_63134417.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap5_63134417.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC/ThucHanh/BaiTap5_63134417/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BaiTap5_63134417.MvcApplication" Language="C#" %>
